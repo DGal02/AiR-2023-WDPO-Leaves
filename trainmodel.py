231eb9c388d4c70433b90b28c696223d7e0c99d5
DGalczyk <dominik.galczyk@gmail.com>
2023-12-31 06:31:13 +0100

    better score 0.51

--- trainmodel.py
+++ trainmodel.py
@@ -5,7 +5,7 @@ print(torch.cuda.get_device_name())
 model = YOLO("yolov8m.pt")
 
 def start():
-    model.train(data="data.yaml", epochs=300, patience=1000, batch=16)
+    model.train(data="data.yaml", epochs=600, patience=800, batch=16)
 
 if __name__ == "__main__":
     start()
